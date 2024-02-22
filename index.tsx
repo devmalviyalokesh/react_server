f001fa8d85a3db59a2cd3fb2cb6536079ead6b6c
lokesh <lokesh.malviya@edgistify.com>
2024-02-22 22:27:14 +0530

    test CI/CD

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import { createElement } from "react";
 import { createRoot } from "react-dom/client";
 
-const title = createElement("h1", null, "React loaded successfully. Updated!");
+const title = createElement("h1", null, "React loaded successfully with CI/CD.");
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 
