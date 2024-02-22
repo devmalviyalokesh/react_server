22d65e55dbe0f802a735a6c6681937e03dcbe096
Lokesh Malviya <lokeshmalviya@192.168.1.3>
2024-02-22 09:54:54 +0530

    react test

---
+++ index.tsx
@@ -0,0 +1,8 @@
+import { createElement } from "react";
+import { createRoot } from "react-dom/client";
+
+const title = createElement("h1", null, "React loaded successfully.");
+
+const root = createRoot(document.getElementById("root") as HTMLElement);
+
+root.render(title);
